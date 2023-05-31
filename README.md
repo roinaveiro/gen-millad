a2993dc63416003ced8f35aace51638898c9ca08
Roi Naveiro <roinaveiro@Rois-MacBook-Air.local>
2023-05-31 12:33:37 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# gen-millad
+# Code for NOR project AItenea - Repsol
+
+## Install `nor` package 
+
+Use 
+
+`pip install -e .`
+
+
+## To Do list
+
+- [ ] Model training/evaluation pipeline
+    - Include scaling option for model evaluation
+    - Include calibration metrics
+
+- [ ] Models
+    - [ ] GP regression
+    - [ ] NGBoost
+    - [ ] Graph Net
+    - [ ] ...
+
+- [ ] Incorporate FQ descriptors
+
+- [ ] Incorporate graph features
+
---
+++ config.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+data_dir = "/Users/roinaveiro/OneDrive - CUNEF/aitenea/conf_greece/data"
+raw_data_path = f'{data_dir}/millad.xlsx'
+mol_col = 'mol'
+smi_col = 'can_SMILES'
+target_column = 'anti-haze2'
+name = 'millad2'
+
+'''
+data_dir = "/Users/roinaveiro/OneDrive - CUNEF/aitenea/conf_greece/data"
+raw_data_path = f'{data_dir}/millad.xlsx'
+mol_col = 'mol'
+smi_col = 'can_SMILES'
+target_column = 'anti-haze5'
+name = 'millad5'
+'''
+
